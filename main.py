dba722ce63a6ba69ae21df42164b5b356abf96dc
kakhramon007 <soatovkahramon3@gmail.com>
2025-02-13 21:53:14 +0500

    Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+
+while True:
+    word = input('Please, type in a word to check or "Quit" to exit: ')
+    if word.isalpha():
+        word = word.upper()
+        reversed_word = word[::-1]
+        if word == 'QUIT':
+            print('\nExit')
+            exit(0)
+        elif word == reversed_word:
+            print('\nThis is palindromic word!')
+            print(f'Version in backwards of word "{word}" is <"{reversed_word}">\n')
+        else:
+            print('\nThis is not palindromic word!')
+            print('Inital word: "{}"'.format(word))
+            print('Reversed version: "{}"\n'.format(reversed_word))
+    else:
+        print('\nError, unknown symbol')
+
+
+
